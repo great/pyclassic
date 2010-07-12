bf70735d1b87b13f38dd53af702811506d0495ca
Intae Kim <greatkit@gmail.com>
2010-07-12 18:34:31 +0900

    .

--- basis/views.py
+++ basis/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import HttpResponse
+from classic.basis.models import Round
+from django.template import Context, loader
+
+def list(request):
+	lesson_rounds = Round.objects.all().order_by("-month")[:12]
+	t = loader.get_template("rounds.html")
+	c = Context({
+		"lesson_rounds": lesson_rounds,
+	})
+
+	return HttpResponse(t.render(c))
+

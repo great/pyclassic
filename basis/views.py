ed6c9b54554ca39fde4c63cca6b87d9b71ef39e1
Intae Kim <greatkit@gmail.com>
2010-07-14 13:45:44 +0900

    filter only archived entries

--- basis/views.py
+++ basis/views.py
@@ -16,7 +16,7 @@ def list_basis(request):
 
 	current_rotation = Rotation.objects.filter(pk=today.strftime("%Y%m"))
 	next_rotation = Rotation.objects.filter(pk=next_month.strftime("%Y%m"))
-	rotations = Rotation.objects.all().order_by("-rotation")[:12]
+	rotations = Rotation.objects.exclude(archive=False).order_by("-rotation")[:12]
 
 	t = loader.get_template("rotations.html")
 	c = Context({
@@ -24,7 +24,6 @@ def list_basis(request):
 		"next_rotation": next_rotation,
 		"rotations": rotations,
 		"today": today,
-		"current_year": today.strftime("%Y"),
 		"current_month": today.strftime("%m"),
 		"next_month": next_month.strftime("%m"),
 	})

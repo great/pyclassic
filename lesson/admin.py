08d6684d282c28e52adfb46d886229fdf79ea524
Intae Kim <greatkit@gmail.com>
2010-08-20 15:27:11 +0900

    admin columns are enhanced refrence to django document

--- lesson/admin.py
+++ lesson/admin.py
@@ -1,7 +1,19 @@
 from classic.lesson.models import Lesson, Teacher, LessonClass
 from django.contrib import admin
 
-admin.site.register(Lesson)
-admin.site.register(Teacher)
-admin.site.register(LessonClass)
+# http://docs.djangoproject.com/en/dev/ref/contrib/admin/
+class LessonAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'alias', 'active',)
+
+class TeacherAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'lesson', 'active',)
+	list_filter = ('lesson',)
+
+class LessonClassAdmin(admin.ModelAdmin):
+	list_display = ('empid', 'name', 'department', 'teacher', 'base', 'operational', 'performance')
+	list_filter = ('teacher',)
+
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(LessonClass, LessonClassAdmin)
 
